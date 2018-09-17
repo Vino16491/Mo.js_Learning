9adcf36cf7129c7d7602c7165b3c55577f354c71
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-17 22:02:44 +0530

    burst added

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const burst = new mojs.Burst({
-    radius: {
-        0: 360
-    },
+    radius: {0: 360},
+    count: 8,
     children: {
-        shape: 'rect',
-        stroke: "teal",
-        strokeWidth: {
-            6: 0
-        },
-        angle: {
-            360: 0
-        },
-        radius: {
-            30: 5
-        },
+        shape: 'cross',
+        stroke: 'cyan',
+        strokeWidth: {6: 0},
+        angle: {220: 0},
+        radius: {10: 2},
         duration: 3000,
 
     }
-}).play();
+})
+
+const burst2 = new mojs.Burst({
+    radius: {0: 360},
+    count: 6,
+    children: {
+        shape: 'zigzag',
+        points:5,
+        stroke: {'orange':'cyan'},
+        strokeWidth: {6: 0},
+        angle: {'-180': 0},
+        radius: {15: 2},
+        opacity:{1:0},
+        duration: 2500,
+
+    }
+})
+
+const timeline = new mojs.Timeline({
+    repeat:999
+}).add(burst, burst2).play();
