4f58a79a296d1e459ca8b522aed6e0bf35cd11ad
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-17 22:46:54 +0530

    created reactive blast

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 const burst = new mojs.Burst({
-    radius: {0: 360},
-    count: 8,
+    radius: {0: 50},
+    count: 45,
     children: {
-        shape: 'cross',
-        stroke: 'cyan',
-        strokeWidth: {6: 0},
-        angle: {220: 0},
-        radius: {10: 2},
+        shape: {'cricle': 'cross'},
+        stroke: {'grey':'cyan'},
+        angle: {720: 0},
+        radius: {15: 2},
         duration: 3000,
+        x:-45,
+        y:-180
 
     }
 })
 
 const burst2 = new mojs.Burst({
-    radius: {0: 360},
-    count: 6,
+    radius: {0: 180},
+    count: 45,
     children: {
-        shape: 'zigzag',
-        points:5,
-        stroke: {'orange':'cyan'},
-        strokeWidth: {6: 0},
-        angle: {'-180': 0},
-        radius: {15: 2},
-        opacity:{1:0},
-        duration: 2500,
-
+        shape: {'rect': 'cross'},
+        stroke: {'grey':'cyan'},
+        strokeSize:10,
+        angle: {360: 0},
+        radius: {10: 2},
+        duration: 1000,
+        delay:2000,
     }
+})
+
+
+const circ = new mojs.Shape({
+    fill:{'white' : 'cyan'},
+    opacity:{1:0}, 
+    radius:{0:40},  
+    duration: 1500,
+    delay:1500,
+})
+
+const circ2 = new mojs.Shape({
+    fill:{'cyan' : 'whitesmoke'}, 
+    radius:{0:90}, 
+    delay:1000,  
+    duration: 1500,
+    opacity:{1:0}, 
+
+})
+
+const circ3 = new mojs.Shape({
+    fill:{'white' : 'cyan'},
+    opacity:{1:0}, 
+    radius:{0:140},  
+    duration: 2000,
+    delay:2000,
 })
 
 const timeline = new mojs.Timeline({
     repeat:999
-}).add(burst, burst2).play();
+}).add(burst, circ, burst2, circ2, circ3).play();
